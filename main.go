a1b6026608818516bab007a8f5052ab2bd219aec
liyuliang <liyuliang2013@gmail.com>
2019-07-26 17:54:01 +0800

    init project

--- main.go
+++ main.go
@@ -5,30 +5,31 @@
 package main
 
 import (
-    "github.com/huichen/wukong/engine"
-    "github.com/huichen/wukong/types"
-    "log"
+	"github.com/huichen/wukong/engine"
+	"github.com/huichen/wukong/types"
+	"log"
 )
 
 var (
-    // searcher是线程安全的
-    searcher = engine.Engine{}
+	// searcher是线程安全的
+	searcher = engine.Engine{}
 )
 
 func main() {
-    // 初始化
-    searcher.Init(types.EngineInitOptions{
-        SegmenterDictionaries: "../data/dictionary.txt"})
-    defer searcher.Close()
+	// 初始化
+	searcher.Init(types.EngineInitOptions{
+		SegmenterDictionaries: "./dictionary.txt"})
 
-    // 将文档加入索引，docId 从1开始
-    searcher.IndexDocument(1, types.DocumentIndexData{Content: "此次百度收购将成中国互联网最大并购"}, false)
-    searcher.IndexDocument(2, types.DocumentIndexData{Content: "百度宣布拟全资收购91无线业务"}, false)
-    searcher.IndexDocument(3, types.DocumentIndexData{Content: "百度是中国最大的搜索引擎"}, false)
+	//defer searcher.Close()
 
-    // 等待索引刷新完毕
-    searcher.FlushIndex()
+	// 将文档加入索引，docId 从1开始
+	searcher.IndexDocument(1, types.DocumentIndexData{Content: "此次百度收购将成中国互联网最大并购"})
+	searcher.IndexDocument(2, types.DocumentIndexData{Content: "百度宣布拟全资收购91无线业务"})
+	searcher.IndexDocument(3, types.DocumentIndexData{Content: "百度是中国最大的搜索引擎"})
 
-    // 搜索输出格式见types.SearchResponse结构体
-    log.Print(searcher.Search(types.SearchRequest{Text: "百度中国"}))
+	// 等待索引刷新完毕
+	searcher.FlushIndex()
+
+	// 搜索输出格式见types.SearchResponse结构体
+	log.Print(searcher.Search(types.SearchRequest{Text: "百度中国"}))
 }
